cea1054d45619482f772e1ccf76a2d3318b233fa
Otavio Henrique <otaviopvaladares@gmail.com>
2018-09-09 20:13:33 -0300

    finishing binary sum

--- binary.py
+++ binary.py
@@ -1,5 +1,5 @@
 class Binary:
-    def __init__(self, number, decimal):
+    def __init__(self, number, decimal = [0]):
         self._number = number
         self._decimal = decimal
 
@@ -13,6 +13,35 @@ class Binary:
         final_decimal = ''.join(str(x) for x in self._decimal)
         return final_decimal
 
+    def max_approximation(self):
+        number = self.add(self.__normalize(self._decimal, [1]))
+        return ''.join(str(x) for x in number)
+
+    def add(self, number):
+        final_number = []
+        decimal_number = self._decimal[::-1]
+        number = number[::-1]
+
+        rest = 0
+
+        for index in range(len(decimal_number)):
+            sum = number[index] + decimal_number[index] + rest
+
+            if sum == 0:
+                final_number.append(0)
+                rest = 0
+            elif sum == 1:
+                final_number.append(1)
+                rest = 0
+            elif sum == 2:
+                final_number.append(0)
+                rest = 1
+            elif sum == 3:
+                final_number.append(1)
+                rest = 1
+
+        return final_number[::-1]
+
     @classmethod
     def convert(cls, number):
         remainder = None
@@ -24,7 +53,7 @@ class Binary:
             result = result // 2
             binary.append(remainder)
 
-        return Binary(binary[::-1], [0])
+        return Binary(binary[::-1])
 
     @classmethod
     def convert_fractional(cls, number, precision=5):
@@ -32,7 +61,7 @@ class Binary:
 
         binary = []
 
-        for _ in range(precision):
+        for _ in range(precision): #TODO: use enumerate() https://stackoverflow.com/questions/522563/accessing-the-index-in-for-loops
             aux = decimal * 2
             if aux > 1:
                 binary.append(1)
@@ -43,3 +72,9 @@ class Binary:
             decimal = aux
 
         return Binary(number, binary)
+
+    def __normalize(self, number1, number2): #TODO: It's need to be something like Binary.normalize(number)
+        for number in range(len(number1) - len(number2)):
+            number2.insert(0, 0)
+
+        return number2
--- tests/binary_test.py
+++ tests/binary_test.py
@@ -8,6 +8,7 @@ class BinaryConversionInteger(TestCase):
     """
     def test_valid_conversion_of_integer_number(self):
         binary = Binary.convert(12343)
+
         self.assertEqual(binary.number, '11000000110111')
 
 class BinaryConversionFractional(TestCase):
@@ -16,8 +17,17 @@ class BinaryConversionFractional(TestCase):
     """
     def test_valid_conversion_of_fractional_number(self):
         binary = Binary.convert_fractional(0.17)
+
         self.assertEqual(binary.decimal, '00101')
 
+class MaxApproximation(TestCase):
+    """
+    Test if it's return the right max approximation of the binary number
+    """
+    def test_valid_max_approximation(self):
+        binary = Binary([0], [0, 0, 1, 0, 1])
+
+        self.assertEqual(binary.max_approximation(), '00110')
 
 if __name__ == '__main__':
     unittest.main()
