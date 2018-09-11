0d61b338a009f27d97b382e6a535fbd2c4fc47ae
Otavio Henrique <otaviopvaladares@gmail.com>
2018-09-11 00:11:03 -0300

    organizing files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Binary error calculator',
+      version='1.0',
+      description='Software to calculate error rate between binary decimal conversion with multiple bits of pricion',
+      author='Otavio Valadares',
+      author_email='otaviopvaladares@gmail.com',
+      url='https://github.com/OtavioHenrique/binary_error_calculator'
+     )
